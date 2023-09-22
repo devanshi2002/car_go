e45c0bfba83a5810e5fdfdaf8378d0ad3bb31593
devanshi2002 <devanshigarg2002@gmail.com>
2023-09-22 21:38:47 +0530

    car booking aap

--- car_go/lib/screens/register_screen.dart
+++ car_go/lib/screens/register_screen.dart
@@ -49,7 +49,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               FirebaseDatabase.instance.ref().child("users");
           userRef.child(currentUser!.uid).set(userMap);
         }
-        await Fluttertoast.showToast(msg: "user has  Successfully Registered");
+        await Fluttertoast.showToast(msg: "user  Successfully Registered");
         Navigator.push(
             context, MaterialPageRoute(builder: (c) => MainScreen()));
       }).catchError((errorMessage) {
